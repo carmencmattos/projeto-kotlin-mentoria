e1186024dce5e05f23dacc428dd68547f74ff7ad
Carmen Mattos <carmencmattos@gmail.com>
2024-01-25 18:57:54 -0300

    criacao do repositorio

---
+++ src/main/kotlin/com/carmencmattos/mentoria/ProjetoKotlinMentoriaApplication.kt
@@ -0,0 +1,11 @@
+package com.carmencmattos.mentoria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoKotlinMentoriaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoKotlinMentoriaApplication>(*args)
+}
---
+++ src/test/kotlin/com/carmencmattos/mentoria/ProjetoKotlinMentoriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.carmencmattos.mentoria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetoKotlinMentoriaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
